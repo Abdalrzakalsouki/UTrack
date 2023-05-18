7981972a9cea5679e6e31df34deb614d35454144
Abdulrazzak Alsssouki <abdulrzakalsssouki@gmail.com>
2023-05-19 01:44:02 +0200

    adding meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,21 @@
 import "./globals.css";
 import { Mulish } from "next/font/google";
+import { Metadata } from "next";
 import Header from "./Header";
 import Footer from "./Footer";
 
 const mulish = Mulish({ subsets: ["latin"], display: "swap" });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "UTrack",
   description: "IP Detection Application",
+  themeColor: "black",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  category: "technology",
 };
 
 export default function RootLayout({
