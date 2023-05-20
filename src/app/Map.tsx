021225c77b482ed86268be8b8aabbd70f596edce
Abdulrazzak Alsssouki <abdulrzakalsssouki@gmail.com>
2023-05-20 18:47:50 +0200

    add auto scrolling

--- src/app/Map.tsx
+++ src/app/Map.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { useRef, useEffect } from "react";
 import styles from "./page.module.scss";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
@@ -11,19 +12,27 @@ const Map = ({
   latitude: number;
   longitude: number;
 }) => {
+  const mapLocation = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (mapLocation.current) {
+      mapLocation.current.scrollIntoView();
+    }
+  }, [mapLocation]);
   return (
-    <MapContainer
-      center={[latitude, longitude]}
-      zoom={13}
-      className={styles.map}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-      />
-      <Marker position={[latitude, longitude]} />
-    </MapContainer>
+    <div ref={mapLocation}>
+      <MapContainer
+        center={[latitude, longitude]}
+        zoom={13}
+        className={styles.map}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+        />
+        <Marker position={[latitude, longitude]} />
+      </MapContainer>
+    </div>
   );
 };
 export default Map;
